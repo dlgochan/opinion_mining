4aa059343ecc010b06c5b2b62a5e741ef6932f71
Haechan Lee <dlgockssla@gmail.com>
2021-11-26 18:18:37 +0900

    add json2df.py

--- crawler/json2df.py
+++ crawler/json2df.py
@@ -1,9 +1,12 @@
 import json
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 
 f_input = open("./rm_dup.json", mode="r")
-f_output = open("df.json", mode="w", encoding='utf8')
+# f_output = open("df.json", mode="w", encoding='utf8')
 str = []
 tweets = []
 
@@ -16,11 +19,16 @@ for j in str:
 ############################################################
 # 이제 tweets 변수에 dict(python의 자료형 중에 하나) 형식으로 json이 담김
 
-for tweet in tweets:
-    tmp = {'text' : remove}
-    tmp2 = json.dumps(tmp) # dict -> json
-    print(tmp['text'], end='\n\n\n')  ## 값 잘 들어갔나 확인용
-    f_output.write(tmp2)
-    f_output.write('\n')
+#json to dataframe
 
-print("END")
+df = pd.DataFrame(columns=['text'])
+i=0
+for tweet in tweets:
+    row = [tweet['text']]
+    series = pd.Series(row, index=df.columns)
+    df = df.append(series, ignore_index=True)
+    if i < 5:
+        print(row)
+        print(df)
+
+    i+=1
