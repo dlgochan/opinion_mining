45acad258cabb43936d01b74ab0520826ab588d8
subin5 <70627533+subin5@users.noreply.github.com>
2021-11-26 21:55:03 +0900

    text pre-processing

--- nlp/okt.py
+++ nlp/okt.py
@@ -1,33 +1,40 @@
-import json
+# -*- conding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
 from konlpy.tag import Okt
+
 okt = Okt()
 
-f_input = open("hanspell.json", mode="r")
-f_output = open("okt.json", mode = "w")
+df = pd.read_csv("hanspell.csv")
+columns = []
+df2 = pd.DataFrame(columns=columns)
 
-str = []
-tweets = []
-
-str = f_input.readlines() 
-
-for j in str:
-    tweets.append(json.loads(j)) # json -> dict
-
-
-result = []
-# 예제
-for j in tweets:
-    words = okt.pos(j['text'])
-    
-    # 명사나 형용사만 추출
+def okt_noun(text):
+    tmp = []
+    words = okt.pos(text)
     for word in words: 
         if word[1] == 'Noun':
-            tmp = {'명사' : word[0]}
-            tmp2 = json.dumps(tmp)
-            f_output.write(tmp2)
-            f_output.write('\n')
+            tmp.append(word[0])
+    return tmp
+
+def okt_verb(text):
+    tmp = []
+    words = okt.pos(text)
+    for word in words: 
+        if word[1] == 'Verb':
+            tmp.append(word[0]) 
+        elif word[1] == 'Adverb':
+            tmp.append(word[0])
         elif word[1] == 'Adjective':
-            tmp = {'형용사' : word[0]}
-            tmp2 = json.dumps(tmp)
-            f_output.write(tmp2)
-            f_output.write('\n')
+            tmp.append(word[0])     
+    return tmp
+    
+    
+df2['okt_noun'] = df['han_text'].apply(lambda x : okt_noun(x))
+df2['okt_verb'] = df['han_text'].apply(lambda x : okt_verb(x)) # verb에 동사, 형용사, 부사
+
+df2.to_csv('words.csv')
+ 
--- nlp/text_cleaning.py
+++ nlp/text_cleaning.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 from hanspell import spell_checker
+from konlpy.tag import Okt
 
 def text_cleaning(text):
     hangul = re.compile('[^ ㄱ-ㅣ가-힣]+') # 한글 정규표현식
@@ -19,17 +20,16 @@ columns = []
 df2 = pd.DataFrame(columns=columns)
 df2['ko_text'] = df['text'].apply(lambda x : text_cleaning(x)) # tweet_text는 받은 데이터 보고 수정하기
 
-print(df2.head())
-
 # ============================ 여기까지 한글 추출
 
 def text_hanspell(text):
     spelled_text = spell_checker.check(text) # 맞춤법 교정
     return spelled_text.checked
     
-
 df3 = pd.DataFrame(columns=columns)
-df3['han_text'] = df2['ko_text'].head().apply(lambda x : text_hanspell(x))
+df3['han_text'] = df2['ko_text'].apply(lambda x : text_hanspell(x))
 
-print(df3.head())
+df3.to_csv("../nlp/hanspell.csv")
     
+# ============================ 여기까지 맞춤법 교정
+
