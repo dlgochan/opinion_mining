de4d114ff2f5cf69c48033648620f8045048d82a
subin5 <70627533+subin5@users.noreply.github.com>
2021-11-27 19:46:56 +0900

    TF-IDF

--- opm/tfidf_sklearn.py
+++ opm/tfidf_sklearn.py
@@ -1,16 +1,24 @@
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TFidfTransformer
+from sklearn.feature_extraction.text import TfidfTransformer
+import pandas as pd
+from konlpy.tag import Okt
 
-index_vectorizer = CountVectorizer(tokenizer = lambda x: get_pos(x))
-X = index_vectorizer.fit_transform(df['ko_text'].tolist())
-print(X.shape)
 
-print(str(index_vectorizer.vocabulary_)[:100]+"..")
-print(df['ko_text'][0])
-print(X[0])
+df = pd.read_csv("../nlp/words2.csv")
 
-tfidf_vectorizer = TFidfTransformer()
+# corpus index 생성
+index_vectorizer = CountVectorizer() # 토큰화, 벡터화를 같이 해줌
+X = index_vectorizer.fit_transform(df['okt_verb']) # 토큰화 이미 해놨으니까 그냥 해놓은거 넣기만 하면 됨
+# (5744, 7269) :  7269의 feature를 가진 5744개의 학습 데이터 셋 
+
+# print(X.shape)
+# print(X[1])
+# print('===================================================')
+
+# TF-IDF
+tfidf_vectorizer = TfidfTransformer()
 X = tfidf_vectorizer.fit_transform(X)
 
-print(X.shape)
-print(X[0])
+# print(X.shape)
+# print(X[1])
+
