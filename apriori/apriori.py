2cd75d8a76d72e2dc9d49b620d93e85e0c76a210
Haechan Lee <dlgockssla@gmail.com>
2021-12-01 20:18:10 +0900

    v1

--- apriori/apriori.py
+++ apriori/apriori.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 #     []
 # ]
 transactions = []
+stop_word = ['위드', '코로나', '단계적', '단계', '일상', '회복', '백신', '패스', '진자']
 df = pd.read_csv("./words2.csv") # 명사 나눈거 가져오기
 tmp = df['okt_noun'] # 각 행을 배열에 담기
 
@@ -23,12 +24,20 @@ for t in transactions:
     transactions[i] = t.split("', '")
     i += 1
     # print(t[0])
-################################################## 이제 transactions 에 각 트윗별 명사들 list형으로 저장됨
+
+for i in range(0, len(transactions)):
+    tmp = []
+    for word in transactions[i]:
+        if(word not in stop_word):
+            tmp.append(word)
+    transactions[i] = tmp
+
+########################################## 이제 transactions 에 각 트윗별 명사들 list형으로 저장됨
 
 # apriori 수행
 results = list(apriori(transactions,  # apriori 파라미터 조절
-            min_support=50/5745,
-            min_confidence=0.6,
+            min_support=100/5745,
+            min_confidence=0.3,
             min_lift=1.0,
             # min_lift=0.8,
             max_length=2))
@@ -63,7 +72,7 @@ for key in counter.keys():
     series = pd.Series(row, index=node_df.columns)
     node_df = node_df.append(series, ignore_index=True)
 
-node_df = node_df[node_df['nodesize'] >= 50]  # node의 최소 frequency 조절
+node_df = node_df[node_df['nodesize'] >= 100]  # node의 최소 frequency 조절
 print(node_df.head())
 #################################################################
 # 아래에서 부터 graph 로 표현
