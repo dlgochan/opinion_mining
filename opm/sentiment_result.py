85846514e9e66485340fdc6098c32c7c44bdbbd7
subin5 <70627533+subin5@users.noreply.github.com>
2021-12-01 19:22:45 +0900

    t

--- opm/sentiment_result.py
+++ opm/sentiment_result.py
@@ -3,16 +3,20 @@ import pandas as pd
 import json
 from pandas import json_normalize
 
-f = open("C:/Users/wngus/Documents/GitHub/opinion_mining/opm/result.json", mode='r',encoding='utf8')
+f = open("./opm/result.json", mode='r')
 
 str = []
 results=[]
 sentiments=[]
 
-str = f.readlines() 
-for j in str:
-    tmp = json.loads(j)
-    sentiments.append(tmp)
+str = f.readlines()
+print(str[0][1:lent]) 
+# for j in str:
+#     tmp = json.loads(j)
+#     sentiments.append(tmp)
 
-for s in sentiments:
-    print(s['document'])
+# sentiments[0] = sentiments[0][1:len(sentiments[0])]
+# print(sentiments[0])
+
+# for s in sentiments:
+#     print(s['document'])
