f2e59b376cb13ac9ea67808be760b41af1bab98e
Haechan Lee <dlgockssla@gmail.com>
2021-12-09 00:25:48 +0900

    add omicron.json

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -8,7 +8,6 @@ access_token_secret = '8in88M4QwrwvXiq6jvYfiJfbakkiPGd6pIxhCHDbXTPGP'
 tweetsPerQry = 100
 maxTweets = 100000
 search = "(백신패스 OR 위드코로나 OR 단계적일상회복) -filter:retweets"
-# search = "(아 OR 요 OR 그 OR 하 OR 그 OR 저) -filter:retweets"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -24,16 +23,16 @@ maxId = -1
 tweetCount = 0
 while tweetCount < maxTweets:
     if(maxId <= 0):
-        newTweets = api.search_tweets(q=search, count=tweetsPerQry, result_type="recent", tweet_mode="extended")
+        newTweets = api.search(q=search, count=tweetsPerQry, result_type="recent", tweet_mode="extended")
     else:
-        newTweets = api.search_tweets(q=search, lang="ko", count=tweetsPerQry, max_id=str(maxId - 1), result_type="recent", tweet_mode="extended")
+        newTweets = api.search(q=search, lang="ko", count=tweetsPerQry, max_id=str(maxId - 1), result_type="recent", tweet_mode="extended")
     if not newTweets:
         print("Tweet Habis")
         break
 
     for tweet in newTweets:
         if(tweet._json['id'] > minId):
-            print(tweet._json['full_text'])
+            # print(tweet._json['full_text'])
             f.write(json.dumps(tweet._json))
             f.write('\n')
 
