cbbce6d24128211da4ba64422dec4dfdcb625173
Haechan Lee <90930393+dlgochan@users.noreply.github.com>
2021-12-21 15:51:55 +0900

    Update crawler.py
    remove API key information for public

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,10 +1,12 @@
 import tweepy
 import json
 
-consumer_key = 'kVY7INg6jazHWIMB9frQp5Ouq'
-consumer_secret = 'iKWYLCSAnafWSsEN5GOseSnLZAQsMPYAmLRnkK5YhY2InCDTa4'
-access_token = '1434698789084745728-zDYXbUNkzG3Hdm2XtMlHSLuOyPIQoT'
-access_token_secret = '8in88M4QwrwvXiq6jvYfiJfbakkiPGd6pIxhCHDbXTPGP'
+// input your tweeter API key
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
+
 tweetsPerQry = 100
 maxTweets = 100000
 search = "(백신패스 OR 위드코로나 OR 단계적일상회복) -filter:retweets"
